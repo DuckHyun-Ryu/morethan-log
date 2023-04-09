6cb763fdb4cc157cf0966c37c6f39700c1f4d279
RYUK <43993474+DuckHyun-Ryu@users.noreply.github.com>
2023-04-09 21:41:13 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "goldduck",
-    image: "/.avtar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avtar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Enterpreneur",
     bio: "I solve world's problem.",
     email: "rdh199993@gmail.com",
